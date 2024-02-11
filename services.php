8d4b912c2574823bbc86bdd80bfa3d26ee978e3e
Matheexx <robotmeex12321@gmail.com>
2024-02-11 14:53:27 -0300

    Iniciando a página dos serviços

---
+++ services.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-gbr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Serviços</title>
+</head>
+<body>
+    <h1>Página de Serviços</h1>
+    <p>Sobre os serviços da empresa...</p>
+</body>
+</html>
