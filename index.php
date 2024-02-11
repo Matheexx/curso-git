bf61fe733e993d4d5278be45bd966fef06c114cc
Matheexx <robotmeex12321@gmail.com>
2024-02-11 12:21:24 -0300

    Mudança no título do site

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Site com git</title>
 </head>
 <body>
-    <?= "<h1>Hello World</h1>" ?>
+    <?= "<h1>Hello World, new change on the project</h1>" ?>
     <?php 
     $var = 5;
     echo "<p>The number is $var</p>";
