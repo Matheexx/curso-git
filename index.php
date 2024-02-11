77d3c553bbfae217abedb5af96248db4001ac449
Matheexx <robotmeex12321@gmail.com>
2024-02-11 12:10:27 -0300

    Criação do arquivo base para o projeto do CursoGit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site com git</title>
+</head>
+<body>
+    <?= "<h1>Hello World</h1>" ?>
+    <?php 
+    $var = 5;
+    echo "<p>The number is $var</p>";
+    ?>
+</body>
+</html>
