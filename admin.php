7beaa8d45073f72677fc00e599f0f704d31e3467
Matheexx <robotmeex12321@gmail.com>
2024-02-12 12:41:02 -0300

    Criando página de admin

---
+++ admin.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Painel</title>
+</head>
+<body>
+    <h1>Painel do Admin</h1>
+    <hr>
+    <p>Sobre o admin e as suas funções</p>
+</body>
+</html>
